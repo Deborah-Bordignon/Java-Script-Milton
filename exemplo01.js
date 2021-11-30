608c2378ebbffa87440eb53fe39cfe05840b7abc
Deborah-Bordignon <94913823+Deborah-Bordignon@users.noreply.github.com>
2021-11-30 11:47:14 -0300

    exercicio

---
+++ exemplo01.js
@@ -0,0 +1,35 @@
+// declaração de variaveis
+// let - variavel local
+
+let nome= 'Deborah';
+
+//var - variavel Global
+ var idade = 31;
+
+ alert ('Nome: ' + nome + '\nIdade: ' +idade );
+
+ 
+
+
+
+
+
+
+
+
+ var teste = 10;
+ teste.toExponential(); // Mostra em notação exponencial
+ teste.toFixed(2);      
+ teste.toPrecision(1);
+ teste.toString();
+
+
+ var teste =  'JavaScript';
+ teste.charAt(4);                 // pegando um caracter de um texto
+ teste.concat('é legal') ;         //concatenando o texto dentro da variavel teste com o texto é legal 
+ teste.split('a');                  // tirando as letras a da palavra dentro de teste e quebrando a palavra tbm tira os espaços e quebra cada palavra contando quantas palavras tem na frase que esta dentro da var
+
+
+
+
+
